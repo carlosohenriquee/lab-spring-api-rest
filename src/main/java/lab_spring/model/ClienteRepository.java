bf2e221707a168371536dcdbd74ab7615f9acc94
Carlos Henrique <henriquezas07@gmail.com>
2025-07-04 18:53:35 -0300

    Initial commit

---
+++ src/main/java/lab_spring/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package lab_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/main/java/lab_spring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package lab_spring.service;
+
+import lab_spring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
